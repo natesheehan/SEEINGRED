e9c7410aebeced8232e1908c6c87c46b66b2abfd
Sheehan <ns651@exeter.ac.uk>
2023-02-24 17:37:10 +0000

    update mobile view of the website

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,27 +19,61 @@
   <style>
 
       body {
+        
         overflow: hidden;
         cursor:grab;
           background-color: #d6d6d6;
           background-image: url("images/logo.png");
-    background-repeat: repeat;
+          border-image-repeat: stretch;  
+  border-image-slice: 30;
     background-size: 18vh;
       }
       .cursor{
           background-image: url("images/cursor for website.png");
       }
+
+      .notepad{
+        margin:auto;display:block;height:100vh;transform: rotate(2deg);position: relative;
+      }
+      .shop-link{
+        margin:auto;display:block;height:70vh;transform: rotate(7deg); top: -120px;left: 35%;position:fixed
+      }
+      .archive-link{
+        margin:auto;display:block;height:70vh;transform: rotate(3deg); top:0px;left: 35%;position:fixed;opacity:0.6
+      }
+      .comic-link{
+        margin:auto;display:block;height:70vh;transform: rotate(9deg); top: 120px;left: 35%;position:fixed;opacity:0.6
+      }
+      .about-link{
+        margin:auto;display:block;height:70vh;transform: rotate(4deg); top: 240px;left: 35%;position:fixed;opacity:0.6
+      }
+
+      @media only screen and (max-width: 600px) {
+        .shop-link{
+        margin:auto;display:block;height:70vh;transform: rotate(7deg); top: -120px;left: 15%;position:fixed
+      }
+      .archive-link{
+        margin:auto;display:block;height:70vh;transform: rotate(3deg); top:0px;left: 15%;position:fixed;opacity:0.6
+      }
+      .comic-link{
+        margin:auto;display:block;height:70vh;transform: rotate(9deg); top: 120px;left: 15%;position:fixed;opacity:0.6
+      }
+      .about-link{
+        margin:auto;display:block;height:70vh;transform: rotate(4deg); top: 240px;left: 15%;position:fixed;opacity:0.6
+      }
+}
+
   </style>
   
   
   <body transition:fade>
       <div class="cursor" style="height:100%;width:100%">
         <div style="position: relative; left: 0; top: 0;">
-          <img src="images/notepad.png" alt="" style="margin:auto;display:block;height:100vh;transform: rotate(2deg);position: relative;">
-          <img src="images/result (1).png" alt="" style="margin:auto;display:block;height:70vh;transform: rotate(7deg); top: -120px;left: 35%;position:fixed">
-          <img src="images/result (2).png" alt="" style="margin:auto;display:block;height:70vh;transform: rotate(3deg); top:0px;left: 35%;position:fixed;opacity:0.4">
-          <img src="images/result (3).png" alt="" style="margin:auto;display:block;height:70vh;transform: rotate(9deg); top: 120px;left: 35%;position:fixed;opacity:0.4">
-          <img src="images/result (4).png" alt="" style="margin:auto;display:block;height:70vh;transform: rotate(4deg); top: 240px;left: 35%;position:fixed;opacity:0.4">
+          <img src="images/notepad.png" alt="" class="notepad">
+          <img src="images/result (1).png" alt="" class="shop-link">
+          <img src="images/result (2).png" alt="" class="archive-link">
+          <img src="images/result (3).png" alt="" class="comic-link">
+          <img src="images/result (4).png" alt="" class="about-link">
         </div>
 
           <img src="images/fronk-chicken-nightmares-3-5.gif" alt="test" style="height: 38vh;position: fixed;bottom: 0;">
